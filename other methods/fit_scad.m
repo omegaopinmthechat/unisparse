49ffb3f89bcbfe6c8e26c8fa6bf3c4ec9add2d29
Amar <onlyaboutcoc@gmail.com>
2026-03-02 19:54:44 +0530

    unified simulation for all

--- other methods/fit_scad.m
+++ other methods/fit_scad.m
@@ -1,9 +1,17 @@
+% What this function does:
+
+% Standardizes the features (so all variables are on the same scale).
+% Centers the target variable (so intercept is handled separately).
+% Uses Ridge regression for a good starting point (since SCAD is non-convex).
+% Minimizes the SCAD objective using fminsearch.
+% Rescales coefficients back to original units.
+% Computes the intercept correctly.
+
 function [beta0_hat, beta_hat] = fit_scad(Xtr, ytr, lambda)
 
 a = 3.7;                      % Fan & Li default
 [n, p] = size(Xtr);
 
-% ---------- STANDARDIZE DESIGN ----------
 muX = mean(Xtr,1);
 sdX = std(Xtr,0,1);
 sdX(sdX == 0) = 1;
@@ -13,10 +21,8 @@ Xs = (Xtr - muX) ./ sdX;
 ybar = mean(ytr);
 yc   = ytr - ybar;
 
-% ---------- GOOD INITIALIZATION (RIDGE) ----------
 beta_init = (Xs' * Xs + lambda * eye(p)) \ (Xs' * yc);
 
-% ---------- OPTIMIZATION (ROBUST SETTINGS) ----------
 opts = optimset('Display','off', ...
                 'MaxIter', 5000, ...
                 'MaxFunEvals', 1e5);
@@ -26,10 +32,8 @@ beta_hat_scaled = fminsearch( ...
     beta_init, ...
     opts);
 
-% ---------- UNSTANDARDIZE ----------
 beta_hat = beta_hat_scaled ./ sdX(:);
 
-% ---------- INTERCEPT ----------
 beta0_hat = ybar - muX * beta_hat;
 
 end
